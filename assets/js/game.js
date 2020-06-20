afbaf0d2e7a6274ee404b1c5c1aede0864654cd7
Taylor Nyquist <tnyquist@gmail.com>
2020-06-20 00:48:27 -0500

    upadated player name null input

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,6 +80,7 @@ var startGame = function() {
         if (playerInfo.health > 0) {
             // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it 
             window.alert("welcome to Robot Gladiators! Round " + (i + 1));
+            //debugger;
         
             // pick new enemy to fight based on the index of the enemy.names array
             var pickedEnemyObj = enemyInfo[i];
@@ -175,9 +176,20 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+        while (name === "" || name === null) {
+            name = prompt("What is your robot's name?");
+        }
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 // playerInfo Object
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
